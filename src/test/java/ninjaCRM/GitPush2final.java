93a9ee14f37027c10e770dbe231c0ac45491ab81
Dell <Dell@DESKTOP-HDDS203>
2025-07-29 08:35:29 +0530

    Merge branch 'master' of https://github.com/GurramTeja/AdvanceSelenium.git

--- src/test/java/ninjaCRM/GitPush2final.java
+++ src/test/java/ninjaCRM/GitPush2final.java
@@ -1,10 +1,10 @@
 package ninjaCRM;
 
-public class GitPush2 {
+public class GitPush2final {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-     System.out.println("git push 2");
+     System.out.println("git push 2final");
 	}
 
 }
